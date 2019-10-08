df659a51a0b7ba16db55b024e7ba604a05e423be
Marcelo Zapatta <marcelo.zapatta@fatec.sp.gov.br>
2019-10-07 23:43:49 -0300

    Alterando a imagem de fundo para a imagem do dia do Bing

--- src/js/components/ImageOfTheDay.jsx
+++ src/js/components/ImageOfTheDay.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
 import '../../css/ImageOfTheDay.css';
 
 export default class ImageOfTheDay extends React.Component {
@@ -9,28 +11,30 @@ export default class ImageOfTheDay extends React.Component {
   getBingImageOfTheDay() {
     // Utilizo a API do bing para buscar a imagem do dia
     fetch('/bing-api/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-BR')
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
+          const { setBingImageOfTheDay } = this.props;
+          setBingImageOfTheDay(result.images[0].url);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+      );
   }
 
   render() {
-    return <h1>Eu sou o bg!</h1>;
+    const { urlBingImageOfTheDay } = this.props;
+    return (
+      <Helmet>
+        <style>{`body { background-image: url(/bing-api/${urlBingImageOfTheDay}); `}</style>
+      </Helmet>
+    );
   }
 }
+
+ImageOfTheDay.propTypes = {
+  setBingImageOfTheDay: PropTypes.func.isRequired,
+  urlBingImageOfTheDay: PropTypes.string,
+};
+
+ImageOfTheDay.defaultProps = {
+  urlBingImageOfTheDay: '',
+};
--- src/js/containers/BingImageOfTheDay.js
+++ src/js/containers/BingImageOfTheDay.js
@@ -5,7 +5,7 @@ import ImageOfTheDay from '../components/ImageOfTheDay';
 const mapStateToProps = (state) => ({ urlBingImageOfTheDay: state.urlBingImageOfTheDay });
 
 const mapDispatchToProps = (dispatch) => ({
-  onComponentDidMount: (url) => dispatch(setBingImageOfTheDay(url)),
+  setBingImageOfTheDay: (url) => dispatch(setBingImageOfTheDay(url)),
 });
 
 const BingImageOfTheDay = connect(
